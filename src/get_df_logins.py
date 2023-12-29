6ee68b386c8de00a21e03313e1a614ad2a51e8cb
theacehome <nikita5413965@mail.ru>
2023-12-29 13:11:03 +0300

    random hight for origin image

--- src/get_df_logins.py
+++ src/get_df_logins.py
@@ -1,7 +1,6 @@
 import pandas as pd
 def get_logins_from_text(txt):
-    text = txt.replace("[becwored] Selected drops on: ", "")  # удалить "Selected drops on:"
-    text = text.split(", ")  # разделить по запятым и превратить в список
+    text = txt.split(", ")  # разделить по запятым и превратить в список
     df = pd.DataFrame(text, columns=['logins'])
 
     # Вывод таблицы
